0982241a5834b76d5e7d5fa0a2a9418528d99b27
Nguyen Quy <nguyenquy.1096@gmail.com>
2018-06-05 13:53:38 +0700

    add Administrator Area

---
+++ QP.BMS.WebApp/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,29 @@
+﻿/// <summary>
+/// <company> Nguyen Quy Blogger </company>
+/// <author> Nguyen Quy </author>
+/// <copyright> Copyright © 2018. All right reserver. </copyright
+/// </summary>
+using System.Web.Mvc;
+
+namespace QP.BMS.WebApp.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
