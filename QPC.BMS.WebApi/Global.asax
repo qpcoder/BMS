7c659cdcd5c1c5aec87661f5c3c99d7f5c93ff8f
Nguyen Quy <nguyenquy.1096@gmail.com>
2018-06-24 00:30:17 +0700

    thêm cái hình cho api project

---
+++ QPC.BMS.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QPC.BMS.WebApi.Global" Language="C#" %>
--- QPC.BMS.WebApi/Global.asax.cs
+++ QPC.BMS.WebApi/Global.asax.cs
@@ -1,24 +1,22 @@
-﻿using System;
+﻿/// <summary>
+/// <company> Nguyen Quy Blogger </company>
+/// <author> Nguyen Quy </author>
+/// <copyright> Copyright © 2018. All right reserver. </copyright
+/// </summary>
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
 
-namespace QPC.BMS.WepApp
+namespace QPC.BMS.WebApi
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class Global : System.Web.HttpApplication
     {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
 
+        protected void Application_Start(object sender, EventArgs e)
+        {
             ///Connfiguration Log4Net
             log4net.Config.XmlConfigurator.Configure();
         }
